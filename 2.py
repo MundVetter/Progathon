9bcd52e10775068c73869e8df0a2fac7967eaa82
Mund Vetter <mundvetter@gmail.com>
2023-12-04 20:46:04 +0100

    array version

--- 2.py
+++ 2.py
@@ -3,20 +3,30 @@ _, m = list(map(float, input().split()))
 numbers = list(map(float, input().split()))
 numbers.sort(reverse=True)
 
-tree = defaultdict(lambda: 0)
-tree[0] = 1
+tree = [1]
 
 prob_best = 0
 for i in range(len(numbers)):
-    current = defaultdict(lambda: 0)
+    nn = len(tree)
+    current = [0] * (nn + 2)
     prob = 0
-    for key, val in tree.items():
-        up = val * numbers[i]
-        down = val * (1 - numbers[i])
-        current[key + 1] += up
-        current[key - 1] += down
+    for j in range(nn):
+        up = tree[j] * numbers[i]
+        down = tree[j] * (1 - numbers[i])
 
-        if key + 1 >= m:
+        jj = j + 1
+
+        current[jj + 1] += up
+        current[jj - 1] += down
+
+
+        # up = val * numbers[i]
+        # down = val * (1 - numbers[i])
+        # current[key + 1] += up
+        # current[key - 1] += down
+        key = jj - ((nn - 1) // 2)
+
+        if key >= m:
             prob += up
         if key - 1 >= m:
             prob += down
