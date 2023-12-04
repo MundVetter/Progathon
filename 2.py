5c357b8f021da8670b4cdacb38fd60025a6477e8
Mund Vetter <mundvetter@gmail.com>
2023-12-04 20:05:41 +0100

    default dict imp.

---
+++ 2.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+_, m = list(map(float, input().split()))
+numbers = list(map(float, input().split()))
+numbers.sort(reverse=True)
+
+print(len(numbers))
+# create a tree of all options
+# two options yes or no and two outcomes
+tree = defaultdict(lambda: 0)
+tree[0] = 1
+
+prob_best = 0
+for i in range(len(numbers)):
+    current = defaultdict(lambda: 0)
+    for key, val in tree.items():
+        current[key + 1] += val * numbers[i]
+        current[key - 1] += val * (1 - numbers[i])
+        # case 2: we lose with prob 1 - numbers[i]
+        # current.append([tree[j][0] - 1, tree[j][1] * (1 - numbers[i])])
+
+    # calculate prob we of having at least m wins, check if prob is higher than pass_m_prob
+    # replace tree with current and pass_m_prob with new prob
+    prob = 0
+    for value in current:
+        if value[0] >= m:
+            prob += value[1]
+    # print(prob, past_prob, tree, current)
+        # total_outcomes += value[1]
+    if prob >= prob_best:
+        prob_best = prob
+    tree = current
+
+print(prob_best)
